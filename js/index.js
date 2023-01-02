347f54950345b5d1fd9a1e5525e7c516d3ef0f64
Ankit Singh <notankit063@gmail.com>
2023-01-02 17:38:58 +0530

    added modal/popup for subsystems

---
+++ js/index.js
@@ -0,0 +1,37 @@
+// var popWindow= document.getElementById("click1");
+
+// popWindow.addEventListener('click',(e)=>{
+//     popWindow.classList.toggle("popup_window");   
+// })
+
+const my_modal = document.getElementById("myModal");
+
+document.addEventListener("DOMContentLoaded", () => {
+    const clickedBtn1 = document.getElementById("myBtn");
+    const clickedBtn2 = document.getElementById("myBtn2");
+    const clickedBtn3 = document.getElementById("myBtn3");
+
+    const closeBtn1 = document.getElementById("myBtn_close");
+    const closeBtn2 = document.getElementById("myBtn_close2");
+    const closeBtn3 = document.getElementById("myBtn_close3");
+
+    clickedBtn1.addEventListener('click',()=>{
+        $("#myModal").modal();
+    });
+    clickedBtn2.addEventListener('click',()=>{
+        $("#myModal2").modal();
+    });
+    clickedBtn3.addEventListener('click',()=>{
+        $("#myModal3").modal();
+    });
+
+    closeBtn1.addEventListener('click',()=>{
+        $("#myModal").modal('hide');
+      });
+    closeBtn2.addEventListener('click',()=>{
+        $("#myModal2").modal('hide');
+      });  
+    closeBtn3.addEventListener('click',()=>{
+        $("#myModal3").modal('hide');
+      });  
+  });
